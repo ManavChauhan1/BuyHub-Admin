8faa083ad6d1414923c866733f9457e09faadf0e
Manav Chauhan <92674597+ManavChauhan1@users.noreply.github.com>
2025-01-25 15:15:30 +0530

    Color Entity for Admin Dashboard Created

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -0,0 +1,30 @@
+import prismadb from '@/lib/prismadb'
+import React from 'react'
+
+import { ColorForm } from './components/color-form';
+
+const ColorPage = async ({
+    params
+}: {
+    params: {colorId: string}
+}) => {
+
+    //Params are changed to awaitedParams
+    const awaitedParams = await params;
+
+    const color = await prismadb.color.findUnique({
+        where: {
+            id: awaitedParams.colorId
+        }
+    });
+
+    return (
+        <div className='flex-col'>
+            <div className='flex-1 space-y-4 p-8 pt-6'>
+                <ColorForm initialData={color}/>
+            </div>
+        </div>
+    )
+}
+
+export default ColorPage
