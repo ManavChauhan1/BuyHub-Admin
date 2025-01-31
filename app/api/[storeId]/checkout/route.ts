c7278ef32d6c905645480c1b568e2f256e2b5318
Manav Chauhan <92674597+ManavChauhan1@users.noreply.github.com>
2025-01-31 22:09:31 +0530

    Frontend URL Error

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -81,7 +81,7 @@ export async function POST(
         }
     });
 
-    return NextResponse.json({ url: session.url }, {
+    return NextResponse.json({ url: session.success_url || session.cancel_url }, {
         headers: corsHeaders
     });
 }
